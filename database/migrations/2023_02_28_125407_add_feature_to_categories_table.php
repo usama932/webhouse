e9c57da422a25166e0d4f86bb87dc25d5348bdbb
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-04-13 14:45:16 +0500

    Update 2023_02_28_125407_add_feature_to_categories_table.php

--- database/migrations/2023_02_28_125407_add_feature_to_categories_table.php
+++ database/migrations/2023_02_28_125407_add_feature_to_categories_table.php
@@ -14,7 +14,7 @@ class AddFeatureToCategoriesTable extends Migration
     public function up()
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->integer('feature')->default(0);
         });
     }
 
