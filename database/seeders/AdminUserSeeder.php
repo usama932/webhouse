8119a96e977b51c6b5410fa683c67412a3f7688e
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-05-10 03:00:25 -0700

    push

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -11,14 +11,14 @@ class AdminUserSeeder extends Seeder
     public function run()
     {
         User::create([
-            
+
             'name' => 'Admin',
             'email' => 'admin@domain.com',
             'password' => bcrypt('123456'),
             'is_admin' =>'1',
         ]);
         User::create([
-            
+
             'name' => 'User',
             'email' => 'user@domain.com',
             'password' => bcrypt('123456'),
